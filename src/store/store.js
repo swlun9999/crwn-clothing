dc8416410e3ae87d49b9a5499fbc2683f8870349
WeiLunWorkShop <weilun1995@live.com>
2022-06-22 00:40:20 +0800

    added redux-persist pattern

--- src/store/store.js
+++ src/store/store.js
@@ -1,17 +1,30 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import { logger } from "redux-logger";
+import { persistReducer, persistStore } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
 import { userReducer } from "./user/user.reducer";
 import { productsReducer } from "./products/products.reducer";
 import { cartReducer } from "./cart/cart.reducer";
 
-export const reducer = {
+const reducers = combineReducers({
   user: userReducer,
   products: productsReducer,
   cart: cartReducer,
+});
+
+const persistConfig = {
+  key: "root",
+  storage,
+  blacklist: ["user"],
 };
 
+const persistedReducer = persistReducer(persistConfig, reducers);
+
 export const store = configureStore({
-  reducer,
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({ serializableCheck: false }).concat(logger),
 });
+
+export const persistor = persistStore(store);
